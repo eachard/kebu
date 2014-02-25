cdfec45e182d0435f2d655d9e318eadf245b26fa
Your Actual Name <laurentcuzin@gmail.com>
2014-02-25 11:35:28 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kebu::Application.config.secret_key_base = '1bd6bda96bfda33c8b9864453a5642ab4ae6ecf3132ac2efd82c42fbf6bae68b22826c078cf8297b4f74f71678b981f26b515a641441849aca8240f6c7352835'
